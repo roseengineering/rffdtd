57dd683afc96284202578f3675e5eb88da2a5ec5
george <george>
2022-03-31 12:32:32 -0400

    write touchstone file using MA type format instead of DB

--- src/rffdtd/touchstone.py
+++ src/rffdtd/touchstone.py
@@ -8,18 +8,17 @@ from .conf import DEFAULT_ZLINE
 def buffer_touchstone(freq, sparam, zline=DEFAULT_ZLINE):
     nfreq = sparam.shape[0]
     nport = sparam.shape[1]
-    with np.errstate(divide='ignore'):
-        db = 20 * np.log10(np.abs(sparam))
+    ma = np.abs(sparam)
     ph = np.angle(sparam, deg=True)
     lines = []
-    lines.append('# HZ S DB R {:.0f}'.format(zline))
+    lines.append('# HZ S MA R {:.0f}'.format(zline))
     for i in range(nfreq):
         buf = '{:<11.4e}'.format(freq[i])
         for m in range(nport):
             if m and nport > 2: buf += '\n{:11s}'.format('')
             for n in range(nport):
                 ix = (i,n,m) if nport == 2 else (i,m,n)
-                buf += '  {:8.3f} {:8.2f}'.format(db[ix], ph[ix])
+                buf += '  {:11.5e} {:8.2f}'.format(ma[ix], ph[ix])
         lines.append(buf)
     lines.append('')
     return '\n'.join(lines)
@@ -39,14 +38,16 @@ def write_touchstone(freq, sparam, zline=DEFAULT_ZLINE, filename=None):
             fi.write(buf)
 
 
-def rect(db, deg):
-    ma = 10**(db / 20)
+def rect(ma, deg, dtype):
+    if dtype == 'db':
+        ma = 10**(ma / 20)
     return ma * np.exp(1j * np.deg2rad(deg))
 
 
 def read_touchstone(filename):
     freq = []
     data = []
+    dtype = None
     with open(filename) as f:
         buf = ''
         while True:
@@ -57,9 +58,10 @@ def read_touchstone(filename):
                     continue
                 if ln[0] == '#':
                     d = ln[1:].lower().split()
-                    assert(d[0] == 'hz' and d[1] == 's' and
-                           d[2] == 'db' and d[3] == 'r' and
-                           d[4] == '50')
+                    dtype = d[2]
+                    assert(d[0] == 'hz' and d[1] == 's'
+                           and d[3] == 'r' and d[4] == '50'
+                           and (dtype == 'db' or dtype == 'ma'))
                     continue
                 if ln[0] == ' ':
                     buf += ln
@@ -67,7 +69,7 @@ def read_touchstone(filename):
             if buf:
                 d = [ float(d) for d in buf.split() ]
                 freq.append(d[0])
-                d = [ rect(d[i], d[i+1]) for i in range(1, len(d), 2) ]
+                d = [ rect(d[i], d[i+1], dtype=dtype) for i in range(1, len(d), 2) ]
                 n = int(np.sqrt(len(d)))
                 d = np.array(d).reshape(n, n)
                 data.append(d.T if d.size == 4 else d)
@@ -76,4 +78,5 @@ def read_touchstone(filename):
     freq = np.array(freq)
     data = np.array(data)
     return freq, data
+
 
